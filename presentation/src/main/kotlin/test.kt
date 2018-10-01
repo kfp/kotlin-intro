886cd60ff99b27da85a5536a033c6af4118cd8e3
Elliott <elliott@localhost.localdomain>
2018-10-01 16:56:26 -0400

    added presentation, moved functions further apart

--- presentation/src/main/kotlin/test.kt
+++ presentation/src/main/kotlin/test.kt
@@ -12,7 +12,26 @@ fun main(args: Array<String>) {
     val (result, color) = multiDeclarations()
 }
 
+
+
+
+
+
+
+
 data class Person (val name: String, var age: Int)  //age is mutable, name is not
+
+
+
+
+
+
+
+
+
+
+
+
 
 val welcome = "Hey Everyone! "
 var person = Person("Bob", 55)  //person is not final
@@ -25,17 +44,53 @@ fun stringInterpolation(){
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 fun ifAsExpression(): Boolean {
     val a = 1
     val eq = if(a == 1) true else false
     return eq;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 fun getAPerson() = person2
 fun nullExplicitType(){
     println("value: ${ifAsExpression().and(false)}")
     println("Initials: ${getAPerson().name.get(0)}")
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 fun automaticCast(obj: Any? = null){    //default value
     if(obj is Int){
@@ -46,11 +101,35 @@ fun automaticCast(obj: Any? = null){    //default value
     println("Null checking ${obj?.hashCode()}. toString is fine ${obj.toString()}")
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 fun whileLoop(){
     do{
         val check = ifAsExpression();
     } while (check)
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 fun forLoop(){
     var i=1;
@@ -59,6 +138,18 @@ fun forLoop(){
         println("i: $i")
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 enum class Color(val rgb: Int) {
     RED(0xFF0000),
@@ -74,15 +165,39 @@ fun whenExample(color: Color?): String{
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 data class Result(val result: Int, val color: Color)
 fun multiDeclarations(): Result{
     return Result(7, Color.GREEN)
-}
+}   //In main
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 data class Money(val amount: BigDecimal, val currency: String)
 fun BigDecimal.percent(percentage: Int) = this.multiply(BigDecimal(percentage)).divide(java.math.BigDecimal(100))   //omitted return type
-fun Int.percentOf(money:Money) = money.amount.multiply(BigDecimal(this)).divide(BigDecimal(100))
+fun Int.percentOf(money:Money) = money.amount.multiply(BigDecimal(this)).divide(BigDecimal(100))    //infix
 fun extensions(){
     println("7 percent of 100: ${BigDecimal(100).percent(7)}")
 
@@ -90,6 +205,16 @@ fun extensions(){
     println("sales tax: ${5.percentOf(car)}")
     //println("sales tax: ${5 percentOf car}")
 }
+
+
+
+
+
+
+
+
+
+
 
 fun extensionProperty(){
     val bd1 = 100.bd
@@ -100,11 +225,27 @@ val Int.bd: BigDecimal
     get() = BigDecimal(this)
 
 
+
+
+
+
+
+
+
 fun String.wut() = this + " lul"
 fun String.toString() = ""
 fun extensions2(){
     println("String is final".wut().toString())
 }
+
+
+
+
+
+
+
+
+
 
 operator fun Color.inc() = when(this){
     Color.RED -> Color.GREEN
@@ -115,6 +256,18 @@ fun overloadOperators(){
     var color = Color.BLUE
     println("Color: ${color++}")
 } //https://kotlinlang.org/docs/reference/operator-overloading.html
+
+
+
+
+
+
+
+
+
+
+
+
 
 fun javaInterop(){
 
